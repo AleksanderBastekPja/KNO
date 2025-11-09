1e945e705d01b47bcd1c2feb432e735951a08b1b
AleksanderBastekPJA <s27454@pjwstk.edu.pl>
2025-11-09 20:29:01 +0100

    fix lab3

---
+++ lab3/classification.py
@@ -0,0 +1,129 @@
+import argparse
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    cols = [
+        "Class", "Alcohol", "Malic acid", "Ash", "Alcalinity of ash", "Magnesium", "Total phenols",
+        "Flavanoids", "Nonflavanoid phenols", "Proanthocyanins", "Color intensity", "Hue",
+        "OD280/OD315 of diluted wines", "Proline"
+    ]
+
+    df = pd.read_csv("lab3/wine/wine.data", header=None, names=cols)
+    df_shuffled = df.sample(frac=1).reset_index(drop=True)
+    print(df_shuffled)
+    wineClasses = df_shuffled["Class"].values
+    hotOneWineClasses = tf.one_hot(wineClasses, 3)
+
+    x = df_shuffled.drop(columns=["Class"])
+    y = hotOneWineClasses
+    X_train, X_test, y_train, y_test = train_test_split(x, y.numpy(), test_size=0.25, random_state=42)
+
+    # MODEL 1
+    epochs_m1 = 80
+    learning_rate_m1 = 0.001
+    batch_size_m1 = 32
+
+    kernel_initializer_m1 = 'random_normal'
+    bias_initializer_m1 = 'zeros'
+
+    model_1 = tf.keras.Sequential([
+        tf.keras.layers.Dense(
+            8,
+            activation='relu',
+            kernel_initializer=kernel_initializer_m1,
+            bias_initializer=bias_initializer_m1
+        ),
+        tf.keras.layers.Dense(16, activation='relu',
+                              kernel_initializer=kernel_initializer_m1,
+                              bias_initializer=bias_initializer_m1),
+        tf.keras.layers.Dense(13, activation='relu',
+                              kernel_initializer=kernel_initializer_m1,
+                              bias_initializer=bias_initializer_m1),
+        tf.keras.layers.Dense(10, activation='relu',
+                              kernel_initializer=kernel_initializer_m1,
+                              bias_initializer=bias_initializer_m1),
+        tf.keras.layers.Dense(8, activation='relu',
+                              kernel_initializer=kernel_initializer_m1,
+                              bias_initializer=bias_initializer_m1),
+        tf.keras.layers.Dense(5, activation='relu',
+                              kernel_initializer=kernel_initializer_m1,
+                              bias_initializer=bias_initializer_m1),
+        tf.keras.layers.Dense(3, activation='softmax',
+                              kernel_initializer=kernel_initializer_m1,
+                              bias_initializer=bias_initializer_m1)
+    ])
+
+    model_1.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=float(learning_rate_m1)),
+        loss=tf.keras.losses.CategoricalCrossentropy(from_logits=False),
+        metrics=['accuracy']
+    )
+
+    history_m1 = model_1.fit(X_train, y_train, epochs=int(epochs_m1), batch_size=batch_size_m1)
+    model_1.evaluate(X_test, y_test)
+
+    # Model 2
+    epochs_m2 = 80
+    learning_rate_m2 = 0.002
+    batch_size_m2 = 64
+
+    kernel_initializer_m2 = 'random_uniform'
+    bias_initializer_m2 = 'he_normal'
+
+    model_2 = tf.keras.Sequential([
+        tf.keras.layers.Dense(
+            8,
+            activation='relu',
+            kernel_initializer=kernel_initializer_m2,
+            bias_initializer=bias_initializer_m2
+        ),
+        tf.keras.layers.Dense(16, activation='relu',
+                              kernel_initializer=kernel_initializer_m2,
+                              bias_initializer=bias_initializer_m2),
+        tf.keras.layers.Dense(13, activation='relu',
+                              kernel_initializer=kernel_initializer_m2,
+                              bias_initializer=bias_initializer_m2),
+        tf.keras.layers.Dense(10, activation='relu',
+                              kernel_initializer=kernel_initializer_m2,
+                              bias_initializer=bias_initializer_m2),
+        tf.keras.layers.Dense(8, activation='relu',
+                              kernel_initializer=kernel_initializer_m2,
+                              bias_initializer=bias_initializer_m2),
+        tf.keras.layers.Dense(5, activation='relu',
+                              kernel_initializer=kernel_initializer_m2,
+                              bias_initializer=bias_initializer_m2),
+        tf.keras.layers.Dense(3, activation='softmax',
+                              kernel_initializer=kernel_initializer_m2,
+                              bias_initializer=bias_initializer_m2)
+    ])
+
+    model_2.compile(
+        optimizer=tf.keras.optimizers.Adadelta(learning_rate=float(learning_rate_m2)),
+        loss=tf.keras.losses.CategoricalCrossentropy(from_logits=False),
+        metrics=['accuracy']
+    )
+
+    history_m2 = model_2.fit(X_train, y_train, epochs=int(epochs_m2), batch_size=batch_size_m2)
+    model_2.evaluate(X_test, y_test)
+
+    plt.plot(history_m1.history['accuracy'], label='Training Accuracy m1')
+    plt.plot(history_m2.history['accuracy'], label='Training Accuracy m2')
+
+    if 'val_accuracy' in history_m1.history:
+        plt.plot(history_m1.history['val_accuracy'], label='Validation Accuracy for m1')
+    if 'val_accuracy' in history_m2.history:
+        plt.plot(history_m2.history['val_accuracy'], label='Validation Accuracy for m2')
+
+    plt.title('Model Accuracy over Epochs for model 2')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.grid(True)
+
+    plt.show()
